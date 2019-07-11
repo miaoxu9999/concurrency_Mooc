849fcc3f499d44d32a351426653e701d2c8b379f
miaoxu9999 <2229009552@qq.com>
2019-07-11 13:38:16 +0800

    tmoicReference的测试

---
+++ src/main/java/com/example/demo/example/atomic/AtomicExample4.java
@@ -0,0 +1,17 @@
+package com.example.demo.example.atomic;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @ClassName AtomicExample4
+ * @Description AtmoicReference的测试
+ * @Author miaoxu
+ * @Date 2019/7/11 13:36
+ * @Version 1.0
+ **/
+public class AtomicExample4 {
+
+    private static AtomicReference<Integer> count = new AtomicReference<>(0);
+
+
+}
