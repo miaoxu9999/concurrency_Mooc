86ce3e4f9c3389b09dab825c6fb6eb55f2353009
miaoxu9999 <2229009552@qq.com>
2019-07-11 23:32:20 +0800

    利用枚举,实现单例

---
+++ src/main/java/com/example/demo/example/singleton/SingletonExample7.java
@@ -0,0 +1,46 @@
+package com.example.demo.example.singleton;
+
+import com.example.demo.annotations.ThreadSafe;
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+/**
+ * @ClassName SingletonExample1
+ * @Description 单例模式例子
+ * 利用枚举
+ * @Author miaoxu
+ * @Date 2019/7/11 22:57
+ * @Version 1.0
+ **/
+@ThreadSafe
+public class SingletonExample7 {
+
+    private SingletonExample7()
+    {
+    }
+
+
+
+    public static synchronized SingletonExample7 getInstance()
+    {
+        return Singletion.INSTANCE.getInstance();
+    }
+
+    private enum Singletion
+    {
+        INSTANCE;
+
+        private SingletonExample7 singleton;
+
+        //JVM 保证这个类只是被调用一次
+        Singletion()
+        {
+            singleton = new SingletonExample7();
+        }
+
+        public SingletonExample7 getInstance() {
+             return singleton;
+        }
+    }
+
+
+}
