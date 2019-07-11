041c54ed9d3cccceca6b47d2f0de682c2b41d0a6
miaoxu9999 <2229009552@qq.com>
2019-07-11 13:40:46 +0800

    atmoicReference的测试代码编写

--- src/main/java/com/example/demo/example/atomic/AtomicExample4.java
+++ src/main/java/com/example/demo/example/atomic/AtomicExample4.java
@@ -1,5 +1,8 @@
 package com.example.demo.example.atomic;
 
+import com.example.demo.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -9,9 +12,20 @@ import java.util.concurrent.atomic.AtomicReference;
  * @Date 2019/7/11 13:36
  * @Version 1.0
  **/
+@Slf4j
+@ThreadSafe
 public class AtomicExample4 {
 
     private static AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public static void main(String[] args) {
+        count.compareAndSet(0, 2);
+        count.compareAndSet(0, 1);
+        count.compareAndSet(1, 3);
+        count.compareAndSet(2, 4);
+        count.compareAndSet(3, 5);
+
+    }
 
 
 }
