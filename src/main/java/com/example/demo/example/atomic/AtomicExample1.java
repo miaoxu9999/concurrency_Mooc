64e8551459b6e8e32847f705565fd07d1f80a130
miaoxu9999 <2229009552@qq.com>
2019-07-11 13:58:47 +0800

    AtomicInteger的使用

---
+++ src/main/java/com/example/demo/example/atomic/AtomicExample1.java
@@ -0,0 +1,57 @@
+package com.example.demo.example.atomic;
+
+import com.example.demo.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @ClassName ConcurrencyTest
+ * @Description AtomicInteger的使用
+ * @Author miaoxu
+ * @Date 2019/7/11 10:43
+ * @Version 1.0
+ **/
+@Slf4j
+@ThreadSafe
+public class AtomicExample1 {
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时请求的线程数量
+    public static int threadTotal = 200;
+
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService  = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() ->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdownNow();
+        log.info("count:{}", count);
+
+    }
+
+    public static void add()
+    {
+        count.incrementAndGet();
+    }
+}
